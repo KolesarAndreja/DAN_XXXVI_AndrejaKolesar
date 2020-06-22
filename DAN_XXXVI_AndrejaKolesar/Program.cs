447a8016302110a2047dca815f8f522483995a38
KolesarAndreja <kolesar.andreja@gmail.com>
2020-06-22 13:50:38 +0200

    t3 and 74 created. LogIntoFile and ReadFromFile implemented

--- DAN_XXXVI_AndrejaKolesar/Program.cs
+++ DAN_XXXVI_AndrejaKolesar/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -12,6 +13,7 @@ namespace DAN_XXXVI_AndrejaKolesar
         private static object locker = new object();
         public static int[,] matrix;
         public static List<int> list = new List<int>(10000);
+        public static string fileName = "OddNumbers.txt";
         static Random rnd = new Random();
 
         public static void MatrixGenerator()
@@ -19,10 +21,13 @@ namespace DAN_XXXVI_AndrejaKolesar
             lock (locker)
             {
                 matrix = new int[100, 100];
+
+                //Wait until list count is less then 10000
                 while (list.Count < 10000)
                 {
                     Monitor.Wait(locker);
                 }
+                //fill the matrix with numbers from list
                 for(int i = 0; i < 100; i++)
                 {
                     for(int j = 0; j < 100; j++)
@@ -38,17 +43,64 @@ namespace DAN_XXXVI_AndrejaKolesar
             int num;
             lock (locker)
             {
+                //add random two-digits number to the list
                 for(int i=0; i<10000; i++)
                 {
                    num =  rnd.Next(10, 100);
                    list.Add(num);
                 }
+                //pulse that you have finished your job with generating numbers
                 Monitor.Pulse(locker);
+            }
+        }
+
+        public static void LogIntoFile()
+        {
+            //get only odd numbers from the matrix
+            int[] arr = list.Where(i => i % 2 == 1).ToArray();
+
+            lock (fileName)
+            {
+                using (StreamWriter sw = File.CreateText(fileName))
+                {
+                    for (int i = 0; i<arr.Length; i++)
+                    {
+                       sw.WriteLine(arr[i]);
+                    }
+                }
+                Monitor.Pulse(fileName);
+            }
+        }
+
+        public static void ReadFromFile()
+        {
+            lock (fileName)
+            {
+                while (!File.Exists(fileName))
+                {
+                    Monitor.Wait(fileName);
+                }
+
+                using (StreamReader sr = File.OpenText(fileName))
+                {
+                    string s;
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.Write(s + " ");
+                    }
+                }
+
             }
         }
 
         static void Main(string[] args)
         {
+
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+
             Thread t1 = new Thread(MatrixGenerator)
             {
                 Name = "Matrix_generator"
@@ -57,22 +109,25 @@ namespace DAN_XXXVI_AndrejaKolesar
             {
                 Name = "Two_digits_number_generator"
             };
-
+            //start first two threads
             t1.Start();
             t2.Start();
-
+            //finish executing first two threads
             t1.Join();
             t2.Join();
 
-            for(int i=0; i<60; i++)
+            Thread t3 = new Thread(LogIntoFile)
             {
-                for(int j = 0; j<60; j++)
-                {
-                    Console.Write( matrix[i,j] + " ");
-                }
-                Console.WriteLine();
-            }
-
+                Name = "odd_numbers_into_file"
+            };
+            Thread t4 = new Thread(ReadFromFile)
+            {
+                Name = "read_odd"
+            };
+            t3.Start();
+            t4.Start();
+            t3.Join();
+            t4.Join();
             Console.ReadKey();
 
 
